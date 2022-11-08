57fce92a60aba9bdc98a86c997b9d581613e3cf4
sMinns <ksua0102@naver.com>
2022-11-09 00:54:01 +0900

    A-B

---
+++ src/stage01/b1001.java
@@ -0,0 +1,24 @@
+package stage01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b1001 {
+    private int a, b;
+    private String str;
+    public b1001() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(a <= 0 || b >= 10) {
+            str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str, " ");
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(a - b);
+    }
+    public static void main(String[] args) throws IOException {
+        new b1001();
+    }
+}
