e72e8c59b0b9441a6a75aec982eb750743c1d9fc
sMinns <ksua0102@naver.com>
2022-11-09 21:17:01 +0900

    Arithmetic operators

---
+++ src/stage01/b10869.java
@@ -0,0 +1,27 @@
+package stage01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b10869 {
+    private int a, b;
+    private String str;
+    public b10869() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new b10869();
+    }
+}
