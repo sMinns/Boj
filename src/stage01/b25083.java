b3a04e5c6dece135f647c0a22adb7fe35c046f9a
sMinns <ksua0102@naver.com>
2022-12-12 23:01:01 +0900

    새싹

---
+++ src/stage01/b25083.java
@@ -0,0 +1,7 @@
+package stage01;
+
+public class b25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+    }
+}
