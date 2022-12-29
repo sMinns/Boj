ca0fd74d7d60a03ec38a1e21a5e1fd509b0d2164
sMinns <ksua0102@naver.com>
2022-12-30 01:48:09 +0900

    A+B - 8

---
+++ src/stage03/b11022.java
@@ -0,0 +1,21 @@
+package stage03;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class b11022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(String.format("Case #%d: %d + %d = %d\n", i+1, a, b, a+b));
+        }
+        bw.flush();
+        bw.close();
+    }
+}
