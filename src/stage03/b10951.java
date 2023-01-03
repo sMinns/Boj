25a28118d8c7b51ef8cb346b65e0ba261d67ee6d
sMinns <ksua0102@naver.com>
2023-01-03 19:03:00 +0900

    A+B - 4

---
+++ src/stage03/b10951.java
@@ -0,0 +1,23 @@
+package stage03;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class b10951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        String str;
+        while ((str = br.readLine()) != null) {
+            st = new StringTokenizer(str);
+            while(st.hasMoreTokens()) {
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                bw.write((a+b) + "\n");
+                bw.flush();
+            }
+        }
+        bw.close();
+    }
+}
